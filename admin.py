608cec17074d343d3851c4e93fca788d7987e4bf
ethan-hunt-stickpk <83871693+ethan-hunt-stickpk@users.noreply.github.com>
2021-08-22 18:36:09 +0700

    2208
    Admin and Security Interface 2208

--- admin.py
+++ admin.py
@@ -43,24 +43,79 @@ def informations():
         return 1
     elif (ans == 'N' or ans == 'n'):
         return 0
-def security():
-    conn = psycopg2.connect(user="postgres", password="1", database="demo", host="localhost", port="5432")
+def sale():
+    conn = psycopg2.connect(user="postgres",
+                                  password="1",
+                                  host="localhost",
+                                  port="5432",
+                                  database="parking_db_last")
     print("Successfully connected!")
     cursor = conn.cursor()
-    postgreSQL_select_Query = "select * from ID"
+    postgreSQL_select_Query = "select * from sale"
     cursor.execute(postgreSQL_select_Query)
-    oldID = cursor.fetchall()
+    sale = cursor.fetchall()
+    print("                  This Monitor Sale             ")
+    print("----------------------------------------------------")
+    for row in sale:
+            print("Day : ",row[0])
+            print("Sale In Day : ",row[1])
+            print("Month : ", row[2])
+            print("Sale In Month : ",row[3])
+            
+    conn.commit()
+    conn.close()
+    print("Do you want to continue[Y/N]? ", end = "")
+    ans = input()
+    if (ans == 'Y' or ans == 'y'):
+        return 1
+    elif (ans == 'N' or ans == 'n'):
+        return 0
+def admin():
+    conn = psycopg2.connect(user="postgres",
+                                  password="1",
+                                  host="localhost",
+                                  port="5432",
+                                  database="parking_db_last")
+    print("Successfully connected!")
+    cursor = conn.cursor()
+    postgreSQL_select_Query = "select * from admin"
+    cursor.execute(postgreSQL_select_Query)
+    admin = cursor.fetchall()
+    print("                  This Monitor Admin             ")
+    print("----------------------------------------------------")
+    for row in admin:
+            print("ID RIF : ",row[0])
+            print("Member ID : ",row[1])
+            print("Full Name : ", row[2])
+            
+    conn.commit()
+    conn.close()
+    print("Do you want to continue[Y/N]? ", end = "")
+    ans = input()
+    if (ans == 'Y' or ans == 'y'):
+        return 1
+    elif (ans == 'N' or ans == 'n'):
+        return 0
+def security():
+    conn = psycopg2.connect(user="postgres",
+                                  password="1",
+                                  host="localhost",
+                                  port="5432",
+                                  database="parking_db_last")
+    print("Successfully connected!")
+    cursor = conn.cursor()
+    postgreSQL_select_Query = "select * from security"
+    cursor.execute(postgreSQL_select_Query)
+    secu = cursor.fetchall()
     print("                  This Monitor Security             ")
     print("----------------------------------------------------")
-    ID = input("Please input RIFID : ")
-    for row in oldID:
-        if(ID != row[0]):
-            print("ID not Enabled")
-        else:
+    for row in secu:
             print("ID RIF : ",row[0])
-            print("Time in : ",row[2])
-            print("Time Out : ", row[3])
-            print("Money Pay : ", row[4])
+            print("Money Pay : ",row[1])
+            print("Transaction id : ", row[2])
+            print("Time in : ",row[3])
+            print("Time Out : ", row[4])
+            
     conn.commit()
     conn.close()
     print("Do you want to continue[Y/N]? ", end = "")
@@ -79,6 +134,8 @@ def menu():
         print("1. Monitoring")
         print("2. Customers Information")
         print("3. Security Screen")
+        print("4. Sale")
+        print("5. Admin")
         print("Your choice: ", end = "")
         choice = input()
         if choice == '1':
@@ -87,6 +144,10 @@ def menu():
             check = informations()
         elif choice == '3':
             check = security()
+        elif choice == '4':
+            check = sale()
+        elif choice == '5':
+            check = admin()
         os.system("clear")
 
 def main():
